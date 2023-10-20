42fb2545650569226d36d9df6867d21421e06c9c
Michael Bass <michael.a.bass@hotmail.co.uk>
2023-10-20 20:12:04 +0100

    feat: Add flask for http control.

--- duck-dns-client-back/app/app.py
+++ duck-dns-client-back/app/app.py
@@ -1,15 +1,16 @@
+import json
 import requests
 import yaml
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, request, Response
 from pathlib import Path
 
 config_path = Path(__file__).parent / "data" / "conf.yml"
 
-sched = BlockingScheduler()
+app = Flask(__name__)
 
 
-@sched.scheduled_job("interval", seconds=10)
-def send_req():
+def send_req() -> str:
     with config_path.open() as config_file:
         config_yaml = yaml.safe_load(config_file.read())
     response = requests.get(
@@ -21,7 +22,32 @@ def send_req():
             "verbose": "true",
         },
     )
-    print(str(response.content, encoding="utf-8"))
+    print(resp := str(response.content, encoding="utf-8"))
+    return resp
 
 
-sched.start()
+@app.route("/update", methods=["GET"])
+def update() -> Response:
+    resp = {"duck_dns_response": send_req()}
+    return Response(status=200, response=json.dumps(resp))
+
+
+@app.route("/config", methods=["GET", "POST"])
+def config() -> Response:
+    if request.method == "GET":
+        with config_path.open() as config_file:
+            config_yaml = yaml.safe_load(config_file.read())
+        return Response(status=200, response=json.dumps(config_yaml))
+    elif request.method == "POST":
+        with config_path.open("w") as config_file:
+            config_file.write(yaml.safe_dump(request.json))
+        resp = {"duck_dns_response": send_req()}
+        return Response(status=200, response=json.dumps(resp))
+
+
+with app.app_context():
+    sched = BackgroundScheduler()
+    sched.add_job(send_req, "interval", minutes=15)
+    sched.start()
+
+app.run(host="0.0.0.0", port=80)
