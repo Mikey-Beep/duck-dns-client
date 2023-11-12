890403d46ada8299d267dd305d299d638255ea75
Michael Bass <michael.a.bass@hotmail.co.uk>
2023-11-12 21:59:51 +0000

    refactor: Linting.

--- duck-dns-client-front/app/app.py
+++ duck-dns-client-front/app/app.py
@@ -1,3 +1,5 @@
+"""This module handles the UI for configuring the DuckDNS client.
+"""
 import json
 import requests
 from flask import Flask, render_template, request, Response
@@ -7,25 +9,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """Serves the homepage.
+    """
     return render_template("index.html")
 
 
 @app.route("/update", methods=["GET"])
 def update() -> Response:
-    url = f"http://back/update"
-    response = requests.get(url)
+    """Proxies the DuckDNS update functionality.
+    """
+    url = "http://back/update"
+    response = requests.get(url, timeout=10)
     return Response(status=200, response=json.dumps(response.json()))
 
 
 @app.route("/config", methods=["GET", "POST"])
 def config() -> Response:
-    url = f"http://back/config"
-    if request.method == "GET":
-        response = requests.get(url)
+    """Proxies the config functionality.
+    """
+    url = "http://back/config"
+    if request.method == "POST":
+        response = requests.post(url, json=request.json, timeout=10)
         return Response(status=200, response=json.dumps(response.json()))
-    elif request.method == "POST":
-        response = requests.post(url, json=request.json)
-        return Response(status=200, response=json.dumps(response.json()))
+    response = requests.get(url, timeout=10)
+    return Response(status=200, response=json.dumps(response.json()))
 
 
 app.run(host="0.0.0.0", port=80)
